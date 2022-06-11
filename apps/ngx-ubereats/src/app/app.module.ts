8645de6ce2a683ac2f6261c5a6787a8e134b73c2
lapinRagnar <lapinragnar@gmail.com>
2022-06-11 21:40:06 +0200

    debut

--- apps/ngx-ubereats/src/app/app.module.ts
+++ apps/ngx-ubereats/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
