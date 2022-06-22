537594a61892805c891298a8f121c556682d9479
lapinRagnar <lapinragnar@gmail.com>
2022-06-22 17:31:34 +0200

    initialisation portfolio

---
+++ apps/portfolio/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nx-monorg-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'portfolio';
+}
