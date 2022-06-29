21c1a8e8781d17c90496f3023f00835c7acd6839
lapinRagnar <lapinragnar@gmail.com>
2022-06-29 20:49:34 +0200

    particle qui ne marche pas - juste un essai

--- apps/portfolio/src/app/components/home/home.component.ts
+++ apps/portfolio/src/app/components/home/home.component.ts
@@ -1,12 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'nx-monorg-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
-  constructor() {}
+export class HomeComponent  {
 
-  ngOnInit(): void {}
+  
+
+
+  
+
+
+
+
+  
 }
