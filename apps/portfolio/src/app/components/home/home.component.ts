f8ed2303b87153a078e3aea70a6310649f4f91a8
lapinRagnar <lapinragnar@gmail.com>
2022-06-30 11:30:30 +0200

    netoyage

--- apps/portfolio/src/app/components/home/home.component.ts
+++ apps/portfolio/src/app/components/home/home.component.ts
@@ -1,19 +1,14 @@
-import { Component} from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'nx-monorg-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent  {
+export class HomeComponent {
 
-  
-
-
-  
-
-
-
-
-  
+ f(x: number){
+  return x /2000
+ }
+ 
 }
--- apps/portfolio/src/assets/js/app.js
+++ apps/portfolio/src/assets/js/app.js
@@ -1,3 +1,1 @@
-particlesJS.load('particles-js', 'assets/js/particles.json', function() {
-    console.log('callback - particles.js config loaded');
-});
+                                                
